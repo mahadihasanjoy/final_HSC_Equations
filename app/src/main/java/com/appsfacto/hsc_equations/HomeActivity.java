4647a7ce602d0150bdf2e60b6e27ab6d4c23928f
mahadihasanjoy <sopnil1995babu@gmail.com>
2016-12-26 19:49:18 +0600

    Initial commit

---
+++ app/src/main/java/com/appsfacto/hsc_equations/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.appsfacto.hsc_equations;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Home);
+
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/appsfacto/hsc_equations/helper/Constant.java
@@ -0,0 +1,10 @@
+package com.appsfacto.hsc_equations.helper;
+
+/**
+ * Created by User on 10/24/2016.
+ */
+
+public final class Constant {
+    public static final String GRID_NO = "grid_no";
+    public static final String TITLE = "title";
+}
